eaeed1b3c7f7397af2abcbb79172a12709636e2b
Glitch (mastodon-webmention-relay) <none>
2018-04-04 17:13:43 +0000

    💪🙀 Updated with Glitch

---
+++ process-status.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const cheerio = require('cheerio');
+const rawSendWebmention = require('send-webmention');
+
+function sendWebmention(source, target, ua) {
+  return new Promise((resolve, reject) => {
+    rawSendWebmention(source, target, ua, (err, result) => err ? reject(err) : resolve(result));
+  }); 
+}
+
+module.exports = async function processStatus({ content, url }) {
+  const $ = cheerio.load(content);
+  const links = [...new Set($('a').toArray().map(a => $(a).attr('href')))]; // Dedupes.
+  
+  let mentionsSent = 0;
+
+  await Promise.all(links.map(async link => {
+    if (!link) {
+      return;
+    }
+    
+    console.log(`Checking link for webmention endpoint: ${link}`);
+    
+    const { success } = await sendWebmention(url, link, 'mastodon-webmention-relay');
+    
+    if (success) {
+      console.log(`Webmention sent for: ${link}`);
+      mentionsSent++;
+    } else {
+      console.log(`Link does not support webmentions: ${link}`);
+    }
+  }));
+  
+  return mentionsSent;
+};
